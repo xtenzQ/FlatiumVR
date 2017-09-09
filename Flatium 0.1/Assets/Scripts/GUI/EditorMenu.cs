fd12e0a5f19112468e51e8436c061736b31e17c1
Letyr <gofablov@gmail.com>
2017-09-10 00:37:03 +0800

    EditorMenu 0.1
    Добавлено EditorMenu, интерфейс, список обьектов. Пока почти нет никаких внутренностей, как и самого редактора.
    Планируется сделать загрузку обьектов при выделении (нужны модели).
    Для пролистывания списка использовать либо свой вариант "липких" слайдеров, либо сделать кнопки прокрутки.
    Также нужно перерисовать весь GUI и возможно пересмотреть его структуру.

---
+++ Flatium 0.1/Assets/Scripts/GUI/EditorMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditorMenu : MonoBehaviour {
+
+	private static GameObject SelectedObject = null;
+	private static Vector3 SELECTED_OBJECT_POSITION = new Vector3 (-20.0f, -11.0f, 0.0f);
+
+	public float rotationSpeed = 0.5f;
+
+	public ScrollRect sr;
+
+	public static void setSelectedObject (ListComponent c) {
+		if (!c) return;
+		GameObject go = Resources.Load (c.url) as GameObject;
+		if (go != null) {
+			EditorMenu.SelectedObject = UnityEngine.Object.Instantiate(go);
+			EditorMenu.SelectedObject.transform.position = SELECTED_OBJECT_POSITION;	
+		}
+	}		
+	
+	void Update () {		
+		if (EditorMenu.SelectedObject != null) {
+			EditorMenu.SelectedObject.transform.rotation *= Quaternion.AngleAxis(rotationSpeed, new Vector3(0.0f, 1.0f, 0.0f));
+		}
+	}
+}
---
+++ Flatium 0.1/Assets/Scripts/GUI/ListComponent.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListComponent : MonoBehaviour {
+
+	private static Color DEFAULT_COLOR = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+	private static Color FOCUS_COLOR = new Color(1.0f, 1.0f, 1.0f, 0.1f);
+	private static Color SELECT_COLOR = new Color(1.0f, 1.0f, 0.0f, 0.1f);
+
+	public static ListComponent selectedComponent = null;
+
+	public string url;
+	public string text {
+		set {
+			textComponent.text = value;
+		}
+
+		get {
+			return textComponent.text;
+		}
+	}
+
+	private Image imageComponent;
+	private Text textComponent;
+	private bool selected = false;
+
+	void Awake () {
+		imageComponent = gameObject.GetComponent<Image> ();
+		textComponent = gameObject.GetComponent<Text> ();
+	}
+
+	public void setFocus(bool focus) {
+		if (!selected) {
+			imageComponent.color = (focus) ? FOCUS_COLOR : DEFAULT_COLOR;
+		}
+		EditorMenu.setSelectedObject ((focus) ? this : ListComponent.selectedComponent);
+	}		
+
+	public void selectComponent () {
+		if (ListComponent.selectedComponent) {
+			ListComponent.selectedComponent.imageComponent.color = DEFAULT_COLOR;
+			ListComponent.selectedComponent.selected = false;
+		}
+		ListComponent.selectedComponent = this;
+		selected = true;
+		imageComponent.color = SELECT_COLOR;
+
+		EditorMenu.setSelectedObject (this);
+	}
+
+}
